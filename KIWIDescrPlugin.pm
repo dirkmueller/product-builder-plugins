848956357470f75817220ee2abc02395a6ed9a72
Dirk Müller <dirk@dmllr.de>
2023-11-07 17:00:17 +0100

    Add zstd

--- KIWIDescrPlugin.pm
+++ KIWIDescrPlugin.pm
@@ -187,6 +187,7 @@ sub createRepositoryMetadata {
     $cmd .= " --excludes=boot/*/*.rpm";
     $cmd .= " --checksum=sha512";
     $cmd .= " --zck";
+    $cmd .= " --general-compress-type=zstd";
     $cmd .= " --no-database";
     $cmd .= " --error-exit-val"; # is not working with old createrepo
     foreach my $repoid (split(/\s+/, $repoids)) {
